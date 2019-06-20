fd73d3bbac21c96050556c01772caf60ebc70d3b
db0company <db0company@gmail.com>
2019-06-20 11:53:17 +0200

    Forgot to commit raw.py

---
+++ starlight/raw.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+############################################################
+# Names
+
+SITE_NAME = 'Starlight Academy'
+LICENSE_NAME = 'Revue Starlight'
+GAME_NAME = LICENSE_NAME
+SMARTPHONE_GAME = 'ReLIVE'
+
+############################################################
+# Per language
+
+SITE_NAME_PER_LANGUAGE = {
+    'ja': u'スタァライト アカデミー',
+    'kr': u'스타라이트 아카데미',
+    'zh-hant': u'星光學院',
+}
+
+GAME_NAME_PER_LANGUAGE = {
+    'ja': u'少女☆歌劇 レヴュースタァライト',
+    'kr': u'소녀가극 레뷰 스타라이트',
+    'zh-hant': u'少女☆歌劇 Revue Starlight',
+}
+
+LICENSE_NAME_PER_LANGUAGE = {
+    'ja': u'レヴュースタァライト',
+    'kr': u'레뷰 스타라이트',
+    'zh-hant': u'Revue Starlight',
+}
+
+SMARTPHONE_GAME_PER_LANGUAGE = {
+    'ja': 'Re LIVE',
+    'kr': u'스타리라',
+    'zh-hant': 'Relive',
+}
